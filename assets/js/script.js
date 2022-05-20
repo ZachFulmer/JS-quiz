16fb111ace8496624dd79bfcaea3aaf9820d6cf1
Zach Fulmer <Zachfulmer@earthlink.net>
2022-05-20 18:24:09 -0500

    added high-score button and timer

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Variable Declarations
+var countTimer = 75;
+
+
+
+// Nav bar that includes "View High Score" link and Timer.
+
+
+// Home Screen with Start Quiz Button
+
+// For loop with question: 4 Answer buttons: Display High-scores (Go-Back/Clear High Scores)
+
+// Submit Score and Enter Initials
+
+// High-Score Page

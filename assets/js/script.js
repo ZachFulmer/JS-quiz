04f87e905e163f51bf882761751fe18cad4704b1
Zach Fulmer <Zachfulmer@earthlink.net>
2022-05-21 17:57:43 -0500

    Complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,47 +11,47 @@ var mainView = document.querySelector("#main");
 var quizObj = [
     {
         question: "Inside which HTML element do we put the JavaScript?",
-        answer1: "<scripting>",
-        answer2: "<js>",
-        answer3: "<javascript>",
-        answer4: "<script>",
-        correctAnswer: "<script>",
+        answer1: "A. <scripting>",
+        answer2: "B. <js>",
+        answer3: "C. <javascript>",
+        answer4: "D. <script>",
+        correctAnswer: "D. <script>",
         answered: false
     },
     {
         question: 'How do you write "Hello World" in an alert box?',
-        answer1: 'msgBox("Hello World")',
-        answer2: 'msg("Hello World")',
-        answer3: 'alertBox("Hello World")',
-        answer4: 'alert("Hello World")',
-        correctAnswer: 'alert("Hello World")',
+        answer1: 'A. msgBox("Hello World")',
+        answer2: 'B. msg("Hello World")',
+        answer3: 'C. alertBox("Hello World")',
+        answer4: 'D. alert("Hello World")',
+        correctAnswer: 'D. alert("Hello World")',
         answered: false
     },
     {
         question: "How to write an IF statement in JavaScript?",
-        answer1: "if i=5 then",
-        answer2: "if (i==5)",
-        answer3: "if i=5",
-        answer4: "if i==5 then",
-        correctAnswer: "if (i==5)",
+        answer1: "A. if i=5 then",
+        answer2: "B. if (i==5)",
+        answer3: "C. if i=5",
+        answer4: "D. if i==5 then",
+        correctAnswer: "B. if (i==5)",
         answered: false
     },
     {
         question: "How does a WHILE loop start?",
-        answer1: "while (i<=10)",
-        answer2: "while (i<=10;i++)",
-        answer3: "while i=1 to 10",
-        answer4: "while (i++)",
-        correctAnswer: "while (i<=10)",
+        answer1: "A. while (i<=10)",
+        answer2: "B. while (i<=10;i++)",
+        answer3: "C. while i=1 to 10",
+        answer4: "D. while (i++)",
+        correctAnswer: "A. while (i<=10)",
         answered: false
     },
     {
         question: "What is the correct way to write a JavaScript array?",
-        answer1: "var colors = ['red','green','blue']",
-        answer2: "var colors = (1:]red], 2:'green', 3:'blue')",
-        answer3: "var colors = 'red', 'green', 'blue'",
-        answer4: "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
-        correctAnswer: "var colors = ['red','green','blue']",
+        answer1: "A. var colors = ['red','green','blue']",
+        answer2: "B. var colors = (1:'red', 2:'green', 3:'blue')",
+        answer3: "C. var colors = 'red', 'green', 'blue'",
+        answer4: "D. var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+        correctAnswer: "A. var colors = ['red','green','blue']",
         answered: false
     }
 ]
@@ -97,7 +97,7 @@ var startQuiz = function()
     mainView.style.justifyContent = "center";
 }
 
-var startBtnHandler = function(event)
+var BtnHandler = function(event)
 {
     // clear the main screen if start button is clicked and proceed to questionaire
     if(event.target.matches("#startButton"))
@@ -123,11 +123,74 @@ var startBtnHandler = function(event)
         mainView.appendChild(answer3);
         mainView.appendChild(answer4);
 
-        updateQuestionionaire();
+        timer = setInterval(function(){
+            if(countTimer > 0)
+            {  
+                countTimer -= 1;
+                document.getElementById("quiz-timer").textContent = "Time: " + countTimer.toString();
+            }
+            else
+            {
+                // clear Interval if at min
+                clearInterval(timer);
+            }
+        }, 1000);
+        updateQuestionionaire(questionCount);
+    }
+
+    // if an answer is selected during the questionaire
+    if(event.target.matches("#A1") || event.target.matches("#A2") || event.target.matches("#A3") || event.target.matches("#A4"))
+    {
+        console.log(event.target.textContent);
+        console.log(quizObj[questionCount].correctAnswer);
+        if(event.target.textContent === quizObj[questionCount].correctAnswer)
+        {
+            // proceed to new question
+            quizObj[questionCount].answered = true;
+            isCorrect(true);
+        }
+        else
+        {
+            countTimer -= 10;
+            isCorrect(false);
+        }
+        
+        // Proceed to next question or end game
+        questionCount++;
+        if(questionCount == quizObj.length)
+        {
+            // Stop Timer to maintain user score
+            clearInterval(timer);
+
+            // Jump to submit score page
+            quizComplete();
+        }
+        else
+        {
+            updateQuestionionaire(questionCount);
+        }
+    }
+
+    if(event.target.matches("#submitInitalsBtn"))
+    {
+        var savedInitials = document.getElementById("submitInitials").value;
+        
+        var removeEl = document.getElementById("DeleteSubmitDiv");
+
+        removeEl.remove();
+
+        localStorage.setItem(savedInitials, countTimer.toString());
+
+        document.querySelector("#main h2").textContent = "High Scores";
+        var highscoreEl = document.createElement("p");
+        highscoreEl.textContent = localStorage.getItem(savedInitials);
+        mainView.appendChild(highscoreEl);
+
+        highscoreEl.textContent = savedInitials + "         " + countTimer.toString();
     }
 }
 
-var updateQuestionionaire = function()
+var updateQuestionionaire = function(questionNumber)
 {
     mainView.style.textAlign = "left";
     mainView.style.alignItems = "flex-start";
@@ -139,22 +202,74 @@ var updateQuestionionaire = function()
     var answer4 = document.getElementById("A4");
 
     // Update Mainview with first question
-    questionEl.textContent = quizObj[0].question;
-    answer1.textContent = "A. " + quizObj[0].answer1;
-    answer2.textContent = "B. " + quizObj[0].answer2;
-    answer3.textContent = "C. " + quizObj[0].answer3;
-    answer4.textContent = "D. " + quizObj[0].answer4;
+    questionEl.textContent = quizObj[questionNumber].question;
+    answer1.textContent = quizObj[questionNumber].answer1;
+    answer2.textContent = quizObj[questionNumber].answer2;
+    answer3.textContent = quizObj[questionNumber].answer3;
+    answer4.textContent = quizObj[questionNumber].answer4;
 
 
 }
 
-// Home Screen with Start Quiz Button
+var isCorrect = function (isTrue)
+{
+    var checkAnswerEl = document.createElement("p");
+    mainView.appendChild(checkAnswerEl);
 
-// For loop with question: 4 Answer buttons: Display High-scores (Go-Back/Clear High Scores)
+    if(isTrue)
+    {
+        checkAnswerEl.textContent = "Correct!";
+    }
+    else
+    {
+        checkAnswerEl.textContent = "Incorrect!";
+    }
+    setTimeout(function(){ checkAnswerEl.remove();}, 1000);
+}
 
-// Submit Score and Enter Initials
+var quizComplete = function()
+{
+    var titleEl = document.querySelector("#main h2");
+    var answer1 = document.getElementById("A1");
+    var answer2 = document.getElementById("A2");
+    var answer3 = document.getElementById("A3");
+    var answer4 = document.getElementById("A4");
 
-// High-Score Page
+    // Clean up from questionaire
+    answer1.remove();
+    answer2.remove();
+    answer3.remove();
+    answer4.remove();
+    mainView.style.textAlign = "center";
+    titleEl.innerHTML = "Your score: " + countTimer.toString() + " </br>Enter your initials to save your score!";
 
-mainView.addEventListener("click",startBtnHandler);
+    var submitInitials = document.createElement("div");
+    var inputInitials = document.createElement("input");
+    var submitBtn = document.createElement("button");
+    submitBtn.className = "buttonGeneral";
+    submitBtn.id = "submitInitalsBtn";
+    submitInitials.className = "submitInitialsDiv";
+    submitInitials.id = "DeleteSubmitDiv";
+    submitBtn.textContent = "Submit";
+    inputInitials.id = "submitInitials";
+    inputInitials.type = "text";
+    inputInitials.size = "10";
+    submitInitials.appendChild(inputInitials);
+    submitInitials.appendChild(submitBtn);
+    mainView.appendChild(submitInitials);
+
+
+}
+
+var highScoreHandler = function(event)
+{
+    if(event.target.matches("#high-score-btn"))
+    {
+        console.log("high-score button was pressed");
+    }
+
+}
+
+hsTimerBar.addEventListener("click",highScoreHandler);
+mainView.addEventListener("click",BtnHandler);
 startQuiz();
